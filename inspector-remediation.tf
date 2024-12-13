050d5c8b45a7ca793fa7ffe7e8c88e597fdba29c
Abdallah <abdallah@rosalyn.ai>
2024-12-13 15:09:32 +0200

    inspector-remediation module

---
+++ inspector-remediation.tf
@@ -0,0 +1,24 @@
+# aws-inspector-remediation
+
+module "inspector_remediation" {
+  source = "./modules/inspector-remediation"
+  organization_id = "<organization_id>"
+  account_id = var.accountid
+  environment_name = var.environment_name
+  cw_event_schedule_expression = "rate(30 minutes)"
+  inspector_sns_email_topic = "<target-channel/email>"
+  lambda_variables = {
+      "ACCOUNT_ID" = var.accountid,
+      "RPT" = "yes",
+      "RPT_CONTROL" = "yes",
+      "ENV" = var.environment_name,
+      "CVE_DAYS_AGE" = 14
+      "TITLE_START_WITH" = "CVE"
+      "REMEDIATION_SLACK_INFO" = "yes",
+      "REMEDIATION_SLACK_ALARM" = "yes",
+      "REMEDIATION_REBOOT_TAG_KEY" = "remediation_reboot",
+      "REMEDIATION_REBOOT_TAG_VALUE" = "true",
+      "REMEDIATION_SCAN_TAG_KEY" = "remediation",
+      "REMEDIATION_SCAN_TAG_VALUE" = "true"
+  }
+}
